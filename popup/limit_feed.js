67086ef4a7b84ab518fe0d0d13664eaa9082c3e5
matthieuDev <matthieu.devaux.epfl@outlook.com>
2023-04-27 18:38:12 +0200

    correct comments

--- popup/limit_feed.js
+++ popup/limit_feed.js
@@ -5,11 +5,7 @@
 function listenForClicks() {
     document.addEventListener("click", (e) => {
 
-    /**
-    * Insert the page-hiding CSS into the active tab,
-    * then get the beast URL and
-    * send a "beastify" message to the content script in the active tab.
-    */
+
     function UpdateLimit(tabs) {
         const limitNbPost = document.getElementById("limitNbPost").value;
         browser.tabs.sendMessage(tabs[0].id, {
@@ -18,17 +14,12 @@ function listenForClicks() {
         });
     }
 
-    /**
-     * Just log the error to the console.
-     */
+
      function reportError(error) {
-        console.error(`Could not beastify: ${error}`);
+        console.error(`Could not send message: ${error}`);
     }
 
-    /**
-    * Get the active tab,
-    * then call "beastify()" or "reset()" as appropriate.
-    */
+ 
     if (e.target.classList.contains("UpdateLimit")) {
         browser.tabs.query({active: true, currentWindow: true})
             .then(UpdateLimit)
@@ -41,12 +32,3 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
     console.error(`lol nope: ${error.message}`);
 }
-
-/**
-* When the popup loads, inject a content script into the active tab,
-* and add a click handler.
-* If we couldn't inject the script, handle the error.
-*/
-browser.tabs.executeScript({file: "/scripts/apply.js"})
-    .then(listenForClicks)
-    .catch(reportExecuteScriptError);
