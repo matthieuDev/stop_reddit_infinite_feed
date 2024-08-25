79657dacd2c2867249130b91bd034d444102bc9e
matthieuDev <matthieu.devaux.epfl@outlook.com>
2024-08-26 01:53:50 +0200

    clean + ignore main pages from now on

--- scripts/background.js
+++ scripts/background.js
@@ -11,13 +11,6 @@ function loadMaxPost () {
 
 let i = 0;
 let maxPost = loadMaxPost();
-let last_after;
-
-let curr_after ;
-let nb_post_next_page ;
-let curr_is_passing_threshold ;
-
-let curr_url;
 
 function filterHtmlThen(tabs, y) {
     browser.tabs.sendMessage(tabs[0].id, {
@@ -34,28 +27,10 @@ filterHtml = () => {
         .then(filterHtmlThen)
         .catch(reportError);
 }
-
-const isGoodBaseUrl = url => {
-    if (!url) return false;
-    return url === 'https://www.reddit.com/' || (url.startsWith('https://www.reddit.com/r/') && url.split('/')[5] !== 'comments');
-} 
-
 function cancel(requestDetails) {
-    // get current url
-    browser.tabs.query({active: true, currentWindow: true})
-        .then(tabs => {
-            curr_url = tabs[0].url;
-        });
-
-    if (!isGoodBaseUrl(curr_url)) return;
 
     filterHtml();
-
-    // When coming to a new page
-    if (isGoodBaseUrl(requestDetails.url)) {
-        i = 0;
-        maxPost = loadMaxPost();
-    } else if (requestDetails.url.startsWith('https://www.reddit.com/svc/shreddit/feeds/home-feed')) {
+    if (requestDetails.url.startsWith('https://www.reddit.com/svc/shreddit/feeds/home-feed')) {
         const distanceList = (requestDetails.url.split('?')[1] ?? '').split('&').filter(v => v.startsWith('distance='));
         if (distanceList.length == 1) {
             i = parseInt(distanceList[0].split('=')[1])
@@ -71,6 +46,6 @@ function cancel(requestDetails) {
 
 browser.webRequest.onBeforeRequest.addListener(
     cancel,
-    {urls: ["https://www.reddit.com/", "https://www.reddit.com/svc/shreddit/feeds/*", "https://www.reddit.com/svc/shreddit/community-more-posts/*", "https://www.reddit.com/r/*/"]},
+    {urls: ["https://www.reddit.com/svc/shreddit/feeds/*", "https://www.reddit.com/svc/shreddit/community-more-posts/*"]},
     ["blocking"],
 );
