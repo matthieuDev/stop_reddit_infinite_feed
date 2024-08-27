705052c4ee16488b7a7b2bd8dcdb3777ffcfa184
matthieuDev <matthieu.devaux.epfl@outlook.com>
2024-08-28 01:55:49 +0200

    best way to stop feed

--- scripts/background.js
+++ scripts/background.js
@@ -12,24 +12,8 @@ function loadMaxPost () {
 let i = 0;
 let maxPost = loadMaxPost();
 
-function filterHtmlThen(tabs, y) {
-    browser.tabs.sendMessage(tabs[0].id, {
-        command: "filterHtml",
-        maxPost: maxPost,
-    });
-}
-function reportError(error) {
-    console.error(`Could not query tab: ${error}`);
-}
-
-filterHtml = () => {
-    browser.tabs.query({active: true, currentWindow: true})
-        .then(filterHtmlThen)
-        .catch(reportError);
-}
 function cancel(requestDetails) {
 
-    filterHtml();
     if (requestDetails.url.startsWith('https://www.reddit.com/svc/shreddit/feeds/home-feed')) {
         const distanceList = (requestDetails.url.split('?')[1] ?? '').split('&').filter(v => v.startsWith('distance='));
         if (distanceList.length == 1) {
@@ -40,8 +24,41 @@ function cancel(requestDetails) {
         if (distanceList.length == 1) {
             i = parseInt(distanceList[0].split('=')[1])
         }
-    } 
-    return {cancel: i > maxPost};
+    }
+
+    if (i > maxPost) {
+        return {cancel: true};
+    }
+
+    let filter = browser.webRequest.filterResponseData(requestDetails.requestId);
+    let decoder = new TextDecoder("utf-8");
+    let encoder = new TextEncoder();
+
+    filter.ondata = (event) => {
+        if (i >= maxPost) return;
+        
+        let str = decoder.decode(event.data, { stream: true });
+        const newI = i + (str.match('</article>')?.length ?? 0) ;
+        if (newI >= maxPost) {
+            let toWrite = '';
+            for (;i < maxPost;i++) {
+                const sizeTag =  '</article>'.length;
+                const posTag = str.indexOf('</article>') + sizeTag;
+                toWrite += str.slice(0, posTag);
+                str = str.slice(posTag);
+            }
+            filter.write(encoder.encode(toWrite));
+            return;
+        }
+        i = newI;
+        filter.write(encoder.encode(str));
+    };
+
+    filter.onstop = (event) => {
+        filter.close();
+    };
+
+    return {cancel: false};
 }
 
 browser.webRequest.onBeforeRequest.addListener(
